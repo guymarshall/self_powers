3f75f9c50854dbde177ef3200bd9fdc79e5e302a
Guy Marshall <guymarshall9@gmail.com>
2022-10-24 16:35:32 +0100

    Add logic for program

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 """
 
 def main():
-    print("Running")
+    number = int(input("Enter a whole number greater than 1: "))
+
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i ** i
+    
+    print(f"Sum of self powers up to {number}: {sum}")
 
 if __name__ == "__main__":
     main()
